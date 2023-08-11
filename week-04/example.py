8463b634ff9b99a90a66a0e34b7870801fdb2f33
Zack <zacharydesario@gmail.com>
2023-08-10 21:11:32 -0400

    adding in class solutions for week 3, lecture notes for week 4, and eample file

---
+++ week-04/example.py
@@ -0,0 +1,21 @@
+names = [
+    'Brendan', 'Peter', 'Methila', 'George',
+    'Artem', 'Evan', 'Brian', 'Alina', 'Faria', 'Daria',
+    'Rahat', 'Sulaiman', 'Yan', 'Khaled', 'Dinora'
+]
+
+# oh this is an example of whatever
+names = 'just zack'
+
+
+
+
+print(names)
+
+
+
+def function(x):
+	print(x)
+	pass
+
+print(x)
